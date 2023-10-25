4fe82aefdaccb5a0fa56f6a8f01ff451c7585236
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2023-10-25 17:04:37 +0530

    Update B1_Synchronization_Problem.java

--- B1_Synchronization_Problem.java
+++ B1_Synchronization_Problem.java
@@ -2,28 +2,25 @@
 import java.util.Vector;
 import java.util.concurrent.Semaphore;
 
-class BoundedBuffer {
-    private Vector<Integer> buffer;
-    private Semaphore empty;
-    private Semaphore full;
-    private Semaphore mutex;
-
-    public BoundedBuffer(int size) {
+class Boundedbuffer{
+    Vector<Integer> buffer;
+    Semaphore empty,mutex,full;
+    Boundedbuffer(int size){
         buffer = new Vector<>(size);
+        mutex = new Semaphore(1);
         empty = new Semaphore(size);
         full = new Semaphore(0);
-        mutex = new Semaphore(1);
     }
-
-    public void produce(int item) throws InterruptedException {
+    
+    public void produce(int item)throws InterruptedException{
         empty.acquire();
         mutex.acquire();
         buffer.add(item);
         mutex.release();
         full.release();
     }
-
-    public int consume() throws InterruptedException {
+    
+    public int consume()throws InterruptedException{
         full.acquire();
         mutex.acquire();
         int item = buffer.firstElement();
@@ -34,58 +31,59 @@ class BoundedBuffer {
     }
 }
 
-class Producer extends Thread {
-    private BoundedBuffer buffer;
-
-    public Producer(BoundedBuffer buffer) {
+class Producer extends Thread{
+    Boundedbuffer buffer;
+    Producer(Boundedbuffer buffer){
         this.buffer = buffer;
     }
-
-    @Override
-    public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
+    
+    public void run(){
+        try{
+            for(int i=0;i<10;i++){
                 buffer.produce(i);
-                Thread.sleep(100); // Simulate work
+                System.out.println("Produced " + i);
+                Thread.sleep(100);
             }
-        } catch (InterruptedException e) {
+        }
+        catch(InterruptedException e){
             Thread.currentThread().interrupt();
         }
     }
 }
 
-class Consumer extends Thread {
-    private BoundedBuffer buffer;
-
-    public Consumer(BoundedBuffer buffer) {
+class Consumer extends Thread{
+    Boundedbuffer buffer;
+    Consumer(Boundedbuffer buffer){
         this.buffer = buffer;
     }
-
-    @Override
-    public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
+    
+    public void run(){
+        try{
+            for(int i=0;i<10;i++){
                 int item = buffer.consume();
-                Thread.sleep(100); // Simulate work
-                System.out.println("Consumed: " + item);
+                Thread.sleep(100);
+                System.out.println("Consumed " + item);
             }
-        } catch (InterruptedException e) {
+        }
+        catch(InterruptedException e){
             Thread.currentThread().interrupt();
         }
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        BoundedBuffer buffer = new BoundedBuffer(5);
-        Producer producer1 = new Producer(buffer);
-        Producer producer2 = new Producer(buffer);
-        Consumer consumer1 = new Consumer(buffer);
-        Consumer consumer2 = new Consumer(buffer);
-
-        producer1.start();
-        producer2.start();
-        consumer1.start();
-        consumer2.start();
-    }
+public class Main
+{
+	public static void main(String[] args) {
+		Boundedbuffer buffer = new Boundedbuffer(5);
+		Producer producer1 = new Producer(buffer); 
+		Consumer consumer1 = new Consumer(buffer); 
+		Producer producer2 = new Producer(buffer); 
+		Consumer consumer2 = new Consumer(buffer); 
+		
+		
+		producer1.start();
+		consumer1.start();
+		producer2.start();
+		consumer2.start();
+	}
 }
