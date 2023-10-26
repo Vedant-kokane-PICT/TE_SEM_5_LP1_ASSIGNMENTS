8a263857deed4c07bb7439902e6e7efeaa845365
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2023-10-26 09:26:29 +0530

    Update Client.java

--- Elective - DS/Assignment_2/Client.java
+++ Elective - DS/Assignment_2/Client.java
@@ -1,21 +1,21 @@
+import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Client {
-    private Client() {
-        // Private constructor
-    }
-
     public static void main(String[] args) {
         try {
-            Registry registry = LocateRegistry.getRegistry();
+            // Obtain a reference to the RMI registry on the server
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+
+            // Look up the remote object by its binding name
             RemoteInterface stub = (RemoteInterface) registry.lookup("RemoteInterface");
+
+            // Invoke the remote method
             String response = stub.sayHello("Alice");
-            System.out.println("Message from the server: " + response);
-        } 
-        catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
+            System.out.println("Message from server: " + response);
+        } catch (Exception e) {
+            System.out.println("Client exception: " + e.getMessage());
         }
     }
 }
