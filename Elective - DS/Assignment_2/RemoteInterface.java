fe087625c8f522f04d6261db667bab5a1adcee11
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2023-10-26 09:26:13 +0530

    Update RemoteInterface.java

--- Elective - DS/Assignment_2/RemoteInterface.java
+++ Elective - DS/Assignment_2/RemoteInterface.java
@@ -1,6 +1,5 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import java.rmi.*;
 
-public interface RemoteInterface extends Remote {
-    String sayHello(String name) throws RemoteException;
+public interface RemoteInterface extends Remote{
+    public String sayHello(String name) throws Exception;
 }
