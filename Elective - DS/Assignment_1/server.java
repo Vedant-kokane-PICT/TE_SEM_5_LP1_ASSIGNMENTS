576536a0240c72f41ba68f4cecb2f0a044f23ecc
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2023-11-18 15:52:17 +0530

    Update server.java

--- Elective - DS/Assignment_1/server.java
+++ Elective - DS/Assignment_1/server.java
@@ -1,29 +1,52 @@
+// Vedant Kokane
+// 31430
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class server{
-    public static void main(String[] args){
-        try{
+public class server {
+    public static void main(String[] args) {
+        try {
             ServerSocket ss = new ServerSocket(6666);
-            Socket s = ss.accept();
-            DataInputStream in = new DataInputStream(s.getInputStream());
+            System.out.println("Server Connection is ready");
+
+            while (true) {
+                Socket s = ss.accept();
+                System.out.println("New client connected: " + s);
+                Thread t = new ClientHandler(s);
+                t.start();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class ClientHandler extends Thread {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            DataInputStream in = new DataInputStream(socket.getInputStream());
             String line = "";
-            System.out.println("Connection is Successfully established");
-            while(!line.equals("over")){
-                try{
+            while (!line.equals("over")) {
+                try {
                     line = in.readUTF();
-                    System.out.println(line);
-                }
-                catch(IOException i){
+                    System.out.println("Received from " + socket + ": " + line);
+                } catch (IOException i) {
                     System.out.println(i);
+                    break;  // Exiting the loop if an exception occurs
                 }
             }
-            System.out.println("Connection is Successfully closed");
-            ss.close();
-        }
-        catch(Exception e){
-             System.out.println(e);
+            System.out.println("Client disconnected: " + socket);
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
